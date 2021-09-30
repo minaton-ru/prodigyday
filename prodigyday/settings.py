a601a1bfb6ab4c3db2d1aa8c294de4b3b5f1ec89
minaton-ru <minaton@yandex.ru>
2021-09-30 21:53:53 +0300

    ready to deploy

--- prodigyday/settings.py
+++ prodigyday/settings.py
@@ -17,10 +17,7 @@ env = environ.Env(
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
