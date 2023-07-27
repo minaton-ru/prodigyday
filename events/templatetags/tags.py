2e9c1e8b9438b187208599200507160815ca4bed
minaton-ru <minaton@yandex.ru>
2023-07-27 22:41:59 +0300

    flake8

--- events/templatetags/tags.py
+++ events/templatetags/tags.py
@@ -8,15 +8,24 @@ from events.models import Event, TextPage
 
 register = template.Library()
 
+
+# создаем кастомный тег для списка всех годов,
+# чтобы использовать в html-шаблонах
 @register.inclusion_tag('tags/all_years_list.html')
-def show_all_years(): # создаем кастомный тег для списка всех годов, чтобы использовать в html-шаблонах
-    all_years_list = set(Event.objects.values_list('year', flat=True)) # все значения поля year (не кортежи), множество убирает дубли
+def show_all_years():
+    # все значения поля year (не кортежи), множество убирает дубли
+    all_years_list = set(Event.objects.values_list('year', flat=True))
     all_years_count = {}
     for year in sorted(all_years_list):
-        all_years_count[year] = Event.objects.filter(year=year).count() # наполняем словарь, ключ - год, значение - количество записей по этому году
-    return { 'all_years_list': all_years_count } # возвращаем словарь
+        # наполняем словарь, ключ - год,
+        # значение - количество записей по этому году
+        all_years_count[year] = Event.objects.filter(year=year).count()
+    return {'all_years_list': all_years_count}  # возвращаем словарь
 
+
+# создаем кастомный тег для списка всех статичных текстовых страниц,
+# чтобы использовать в html-шаблонах
 @register.inclusion_tag('tags/text_pages_list.html')
-def show_all_text_pages(): # создаем кастомный тег для списка всех статичных текстовых страниц, чтобы использовать в html-шаблонах
+def show_all_text_pages():
     all_text_pages_list = TextPage.objects.all()
-    return { 'all_text_pages_list': all_text_pages_list }
+    return {'all_text_pages_list': all_text_pages_list}
