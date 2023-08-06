c9e331f142abecf9c8499da28b9d75132b8b862b
minaton-ru <minaton@yandex.ru>
2023-08-06 17:14:50 +0300

    flake8 and docstrings

--- events/views.py
+++ events/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from datetime import date
 from django.template import loader
 from django.http import HttpResponse
-from datetime import date # импортируем модуль для работы с датой
 from events.models import Event, TextPage
 
-month_name_tuple = ('января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря') # глобальный кортеж с названиями месяцев
+# Глобальный кортеж с названиями месяцев
+month_name_tuple = ('января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+                    'июля', 'августа', 'сентября', 'октября', 'ноября',
+                    'декабря')
+
 
 def how_many_years(years_count):
+    '''
+    Функция для определения множественного числа года.
+    Принимает число - год.
+    Возвращает строку.
+    '''
     lastDigit = years_count % 10
     if lastDigit == 1:
         return f"{years_count} год назад"
@@ -15,39 +23,58 @@ def how_many_years(years_count):
     else:
         return f"{years_count} лет назад"
 
-def index(request): # главная страница
-    todayDate = date.today() # берем сегодняшнюю дату
+
+def index(request):
+    '''Главная страница'''
+    # Берем сегодняшнюю дату
+    todayDate = date.today()
     template = loader.get_template('index.html')
-    this_month_name = month_name_tuple[todayDate.month - 1] # берем название текущего месяца из кортежа
-    this_day_list = Event.objects.filter(day=todayDate.day, month=todayDate.month) # фильтруем события по текущему дню и месяцу формат int
-    context = { 'this_day_list': this_day_list,
-    'this_day': todayDate.day,
-    'this_month': this_month_name, }
+    # Берем название текущего месяца из кортежа
+    this_month_name = month_name_tuple[todayDate.month - 1]
+    # Фильтруем события по текущему дню и месяцу формат int
+    this_day_list = Event.objects.filter(day=todayDate.day,
+                                         month=todayDate.month)
+    context = {'this_day_list': this_day_list,
+               'this_day': todayDate.day,
+               'this_month': this_month_name}
     return HttpResponse(template.render(context))
 
-def year_list(request, events_year): # для страницы всех событий одного года
-    todayDate = date.today() # берем сегодняшнюю дату
+
+def year_list(request, events_year):
+    '''Для страницы всех событий одного года'''
+    # Берем сегодняшнюю дату
+    todayDate = date.today()
     template = loader.get_template('year_list.html')
-    year_events_list = Event.objects.filter(year=events_year) # фильтруем события по году, который принимает функция
+    # Фильтруем события по году, который принимает функция
+    year_events_list = Event.objects.filter(year=events_year)
     years_count = todayDate.year - events_year
-    context = { 'year_events_list': year_events_list,
-    'how_many_years': how_many_years(years_count),
-    'events_year': events_year} # возвращаем в контекст год, который приняла функция
+    context = {'year_events_list': year_events_list,
+               'how_many_years': how_many_years(years_count),
+               # возвращаем в контекст год, который приняла функция
+               'events_year': events_year}
     return HttpResponse(template.render(context))
 
-def day_detail(request, event_month, event_day): # для страницы всех событий одного дня
+
+def day_detail(request, event_month, event_day):
+    '''Для страницы всех событий одного дня'''
     template = loader.get_template('day_detail.html')
-    this_month_name = month_name_tuple[event_month - 1] # берем название нужного месяца из кортежа
-    day_events_list = Event.objects.filter(day=event_day, month=event_month) # фильтруем события по дню и месяцу, которые принимает функция
-    context = { 'day_events_list': day_events_list,
-    'events_day': event_day, # возвращаем в контекст день, который приняла функция, и название месяца
-    'events_month': this_month_name }
+    # Берем название нужного месяца из кортежа
+    this_month_name = month_name_tuple[event_month - 1]
+    # Фильтруем события по дню и месяцу, которые принимает функция
+    day_events_list = Event.objects.filter(day=event_day, month=event_month)
+    context = {'day_events_list': day_events_list,
+               # возвращаем в контекст день, который приняла функция,
+               # и название месяца
+               'events_day': event_day,
+               'events_month': this_month_name}
     return HttpResponse(template.render(context))
 
-def text_page(request, page): # Статичные текстовые страницы
+
+def text_page(request, page):
+    '''Статичные текстовые страницы'''
     template = loader.get_template('text_page.html')
     page = TextPage.objects.get(page=page)
     page_text = page.text
     page_title = page.title
-    context = { 'page_text': page_text, 'page_title': page_title }
+    context = {'page_text': page_text, 'page_title': page_title}
     return HttpResponse(template.render(context))
